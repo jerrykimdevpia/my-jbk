c217ce7cec4c8a855cc38214d0314d75ea9a5e04
jerrykim <jerrykimdevpia@gmail.com>
2016-11-21 21:41:34 +0900

    first commit

---
+++ src/main/java/jerry/MyJbkApplication.java
@@ -0,0 +1,12 @@
+package jerry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJbkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJbkApplication.class, args);
+	}
+}
